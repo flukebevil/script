dadce1aae775eb5a2967172dc5e8b91bf04371a5
sarawoot pokkam <flukebevil@gmail.com>
2019-03-08 10:43:52 +0700

    1st initial

---
+++ dumpLink.js
@@ -0,0 +1,14 @@
+const util = require('./helper/shellHelper');
+const fs = require('fs');
+
+const readLinkFile = fs.readFileSync(__dirname + '/totalOfficeURL.json');
+const urlList = JSON.parse(readLinkFile);
+
+// urlList.forEach(async elem => {
+//   await util.callPython(elem.link, elem.link);
+// });
+const init = async () => {
+  await util.callPython('www.20scoops.com', '20scoops');
+};
+init();
+console.log(urlList);
