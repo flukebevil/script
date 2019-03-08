9154f713c7335d8b14c1bf0e239a831389689e4b
sarawoot pokkam <flukebevil@gmail.com>
2019-03-08 12:01:25 +0700

    fixed py function

--- link.py
+++ link.py
@@ -8,6 +8,9 @@ with open(base_path+'/totalOfficeURL.json') as f:
     data = json.load(f)
 
 for url in data:
-    bash = "wget --spider -r "+data['link']+" 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > "+data.link+".txt"
+    bash = "wget --spider -r " + \
+        url['link'] + \
+        " 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > " + \
+        url['link']+".txt"
     process = subprocess.Popen(bash.split(), stdout=subprocess.PIPE)
     output, error = process.communicate
