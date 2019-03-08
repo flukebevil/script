1a1b780fa9cf7aef221aa6359aad8b9019ca91a8
sarawoot pokkam <flukebevil@gmail.com>
2019-03-08 11:17:47 +0700

    fix

--- helper/shellHelper.js
+++ helper/shellHelper.js
@@ -1,19 +1,21 @@
-const spawn = require('child_process').spawn;
+const spawn = require("child_process").spawn;
 
 const callPython = (url, fileName) => {
   return new Promise(resolve => {
-    const callCSV = spawn('wget', [
+    const callCSV = spawn("wget", [
       // '--spider -r ' +
       //   url +
       //   " 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '.(css|js|png|gif|jpg|JPG)$' > " +
       //   fileName +
       //   '.txt'
-      "wget --spider -r "+url+" 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > urls.txt"
+      "--spider -r " +
+        url +
+        " 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '.(css|js|png|gif|jpg|JPG)$' > urls.txt"
     ]);
-    callCSV.stdout.on('data', data => {
+    callCSV.stdout.on("data", data => {
       console.log(data);
     });
-    callCSV.on('exit', () => {
+    callCSV.on("exit", () => {
       resolve();
     });
   });
