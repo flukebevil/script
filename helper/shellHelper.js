d1d5130140ec4f899b1cd611351f6b22dc6abdf2
sarawoot pokkam <flukebevil@gmail.com>
2019-03-08 11:16:36 +0700

    test

--- helper/shellHelper.js
+++ helper/shellHelper.js
@@ -1,14 +1,14 @@
 const spawn = require('child_process').spawn;
-const exec = require('child_process').exec;
 
 const callPython = (url, fileName) => {
   return new Promise(resolve => {
     const callCSV = spawn('wget', [
-      '--spider -r ' +
-        url +
-        " 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '.(css|js|png|gif|jpg|JPG)$' > " +
-        fileName +
-        '.txt'
+      // '--spider -r ' +
+      //   url +
+      //   " 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '.(css|js|png|gif|jpg|JPG)$' > " +
+      //   fileName +
+      //   '.txt'
+      "wget --spider -r "+url+" 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg\|JPG\)$' > urls.txt"
     ]);
     callCSV.stdout.on('data', data => {
       console.log(data);
---
+++ link.py
@@ -0,0 +1,10 @@
+import urllib2
+import re
+
+# connect to a URL
+website = urllib2.urlopen('https://www.20scoops.com')
+# read html code
+html = website.read()
+# use re.findall to get all the links
+links = re.findall('"((http|ftp)s?://.*?)"', html)
+print links
